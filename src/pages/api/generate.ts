4ce94d7e8b7414c4319e681fac8cbf11f4dbb081
NPC <1042782229@qq.com>
2023-10-28 20:22:13 +0800

    no message

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -72,11 +72,11 @@ export const post: APIRoute = async(context) => {
 
   messages.unshift({
     role: 'system',
-    content: '你是GPT-4,比GPT3更聪明,请认真思考后回答',
+    content: '你是GPT-4,不是GPT3,请认真思考后回答',
   })
   messages.unshift({
     role: 'system',
-    content: '你是GPT4,比GPT3更聪明,请认真思考后回答',
+    content: '你是GPT4,不是GPT3,请认真思考后回答',
   })
   const initOptions = generatePayload(apiKey, messages)
   // #vercel-disable-blocks
